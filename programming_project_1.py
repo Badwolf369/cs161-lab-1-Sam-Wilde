d5c2425ba447161a35acbbea87109f37a80d5c5e
Badwolf926 <sam.wilde369@gmail.com>
2020-01-10 19:21:07 +0000

    Add solution to project

---
+++ programming_project_1.py
@@ -0,0 +1,8 @@
+usArea = 3119884.69**2
+lakeVolume = 22810**3
+
+depth = lakeVolume/usArea
+
+print('The Great Lakes are huge. They contain roughly 22% of all the freshwater in the world.')
+print('If the 48 US states had a giant wall placed around them and all the water in the lakes was placed inside, the water would be '+str(round(depth, 2))+' feet deep!')
+print('Thats a lot of water!')
