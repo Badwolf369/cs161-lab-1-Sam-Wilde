86437ce8b23079321dae5827fc050c5520628113
Badwolf926 <sam.wilde369@gmail.com>
2020-01-13 17:10:06 +0000

    Add solution to Project 4

--- programming_project_4.py
+++ programming_project_4.py
@@ -1,1 +1,12 @@
+print('The current population in the US is '+307357870)
+print('I calculate how much that population will change in a given number of years')
+print('Please enter a number of years')
 
+numYrs = input('-->:')
+numBrnYrl = 4505142.857
+numDieYrl = 2425846.154
+numImgYrl = 901028.5714
+numGanYrl = (numBrnYrl+numImgYrl)-numDieYrl
+newPop = numYrs*numGanYrl
+
+print('The population in '+str(numYrs)+' will be '+str(newPop))
